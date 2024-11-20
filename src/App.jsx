070ca9fb59f906b849584e853affc46789db2b26
MERN TRAINER <futuralabs.mern@gmail.com>
2024-11-20 16:20:11 +0530

    added: password generator

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Fragment } from "react"
 import Cards from "./Pages/Cards"
 import LandingPage from "./Pages/LandingPage"
 import States from "./Pages/states"
+import PasswordGenerator from "./Pages/PasswordGenerator"
 
 const App = () => {
     return <Fragment>
         {/* <Cards /> */}
         {/* <LandingPage /> */}
-        <States />
+        {/* <States /> */}
+        <PasswordGenerator />
     </Fragment>
 }
 
---
+++ src/Pages/PasswordGenerator.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react"
+
+const PasswordGenerator = () => {
+
+    const [password, setPassword] = useState("")
+    const [history, setHistory] = useState([])
+
+    const createRandomPassword = () => {
+        const strings = [
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', // Uppercase letters
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', // Lowercase letters
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',                                               // Numbers
+            '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', '|', ';', ':', "'", '"', ',', '.', '<', '>', '/', '?', '`', '~' // Special characters
+        ];
+        let len = 0;
+        let generatedPassword = ""
+        while (len < 16) {
+            const randomIndex = Math.floor(Math.random() * strings.length)
+            generatedPassword += strings[randomIndex]
+            len++
+        }
+        setPassword(generatedPassword)
+        setHistory([...history, generatedPassword])
+    }
+
+    return <div className="d-flex justify-content-center align-items-center flex-column mt-4">
+        {password}
+        <button onClick={createRandomPassword}>Generate Password</button>
+        {history.join(" , ")}
+    </div>
+}
+
+export default PasswordGenerator
