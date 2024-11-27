489479ecc83f84e32c3c0842f9cd1a1ea61c2fa8
MERN TRAINER <futuralabs.mern@gmail.com>
2024-11-27 15:04:39 +0530

    added: context api

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
-import { Fragment } from "react"
+import { createContext, Fragment, useState } from "react"
 // import PasswordGenerator from "./Pages/PasswordGenerator"
 import TodoPage from "./Pages/TodoPage"
 import { Toaster } from "react-hot-toast"
+import Context from "./Components/Context"
 // import ParentComponent from "./Pages/ParentComponent"
 
+export const myContext = createContext()
+
 const App = () => {
+
+    const [counter, setCounter] = useState(0)
+
     return <Fragment>
         {/* <Cards /> */}
         {/* <LandingPage /> */}
         {/* <NavBar /> */}
         {/* <States /> */}
         {/* <PasswordGenerator /> */}
-        <TodoPage />
+        {/* <TodoPage /> */}
+        <myContext.Provider value={{ counter, setCounter }}>
+            <Context />
+        </myContext.Provider>
         {/* <ParentComponent /> */}
         <Toaster position="top-right"/>
     </Fragment>
---
+++ src/Components/Context.jsx
@@ -0,0 +1,18 @@
+// state => js object to handle informations or data in a component
+// props => parent to child 
+// context api => Global state management
+
+import { useContext } from "react"
+import { myContext } from "../App"
+
+const Context = () => {
+
+    const { counter, setCounter } = useContext(myContext)
+
+    return <div className="d-flex align-items-center flex-column gap-2 mt-3">
+        <div>{counter}</div>
+        <button className="btn btn-danger btn-sm" onClick={() => setCounter(counter + 1)}>Increment</button>
+    </div>
+}
+
+export default Context
